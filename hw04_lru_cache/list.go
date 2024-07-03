43f02538bd0e22d3445e72def0c806aa0f9953be
w0lframm <spytwig@yandex.com>
2024-07-03 14:23:30 +0300

    Add `list` type implementation and tests for it.
    `list` type implements `List` interface.
    Added tests for lists with one and two elements.
    
    Signed-off-by: w0lframm <spytwig@yandex.com>

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,93 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front  *ListItem
+	back   *ListItem
+	length int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+	}
+	if l.length == 0 {
+		l.front = item
+		l.back = item
+	} else {
+		item.Next = l.front
+		l.front.Prev = item
+		l.front = item
+	}
+	l.length++
+	return l.front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	old := l.back
+	l.back = &ListItem{
+		Value: v,
+		Prev:  old,
+	}
+	l.length++
+	if l.length == 1 {
+		l.front = l.back
+	} else {
+		old.Next = l.back
+	}
+	return l.back
+}
+
+func (l *list) Remove(i *ListItem) {
+	l.length--
+	if i.Next == nil && i.Prev == nil {
+		l.front = nil
+		l.back = nil
+		return
+	}
+	prev, next := i.Prev, i.Next
+	if prev == nil {
+		next.Prev = nil
+		l.front = next
+		return
+	}
+	if next == nil {
+		prev.Next = nil
+		l.back = prev
+		return
+	}
+	prev.Next, next.Prev = next, prev
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	prev, next := i.Prev, i.Next
+	if prev == nil && next == nil {
+		return
+	}
+	if prev == nil {
+		return
+	}
+	if next == nil {
+		l.back = prev
+	}
+	prev.Next = next
+	i.Prev = nil
+	i.Next = l.front
+	l.front.Prev = i
+	l.front = i
 }

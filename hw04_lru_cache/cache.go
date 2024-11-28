0e621d2dd53eb7993c3b1699ee0f5d27a1bd32a2
w0lframm <spytwig@yandex.com>
2024-11-28 16:35:59 +0300

    Add `cache` type implementation and tests for it.
    Signed-off-by: w0lframm <spytwig@yandex.com>

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,14 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type cacheValue struct {
+	key   Key
+	value interface{}
 }
 
 func NewCache(capacity int) Cache {
@@ -22,4 +25,43 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	v, exist := c.items[key]
+	if exist {
+		v.Value = cacheValue{
+			key:   key,
+			value: value,
+		}
+		c.queue.MoveToFront(v)
+		return true
+	}
+	if c.queue.Len() == c.capacity {
+		item := c.queue.Back()
+		c.queue.Remove(item)
+		delete(c.items, item.Value.(cacheValue).key)
+	}
+	cValue := cacheValue{
+		key:   key,
+		value: value,
+	}
+	v = c.queue.PushFront(cValue)
+	c.items[key] = v
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	v, exist := c.items[key]
+	if exist {
+		c.queue.MoveToFront(v)
+		return v.Value.(cacheValue).value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
